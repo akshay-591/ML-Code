88a9dc00d9bb557822d304a1aca030804eeb9433
akshay-591 <64590922+akshay-591@users.noreply.github.com>
2020-09-21 14:14:46 +0530

    Bugs fixed

--- Python/Tips&Tricks/MLTricks/main.py
+++ Python/Tips&Tricks/MLTricks/main.py
@@ -2,7 +2,7 @@
 
 import numpy as mat
 from matplotlib import pyplot
-from scipy import io, optimize
+from scipy import io
 
 from MLTricks import Regularised_Linear_Cost_Grad as Rg, leaning_curve, Poly_mapping, feature_normalization, ployfit
 
@@ -77,8 +77,8 @@ pyplot.ylim(0, 150)
 pyplot.legend(["training error", "Validation error"])
 pyplot.show()
 
-# we can see in the graph the error is increasing with the number of training example and
-# also error is high in cross validation set.
+# we can see in the graph the error is increasing with the number of training example increasing and also error is
+# high in cross validation set that means adding more example  will not going to solve the problem.
 
 # To solve this problem we can add some more features to overcome the under fitting
 # we can create new features using polynomial regression.
@@ -89,10 +89,8 @@ pyplot.show()
 # mapping and normalizing training set
 power = 8
 X_mapped = Poly_mapping.map(X, power)
-# print(X_mapped)
 # normalize or scaling the mapped input matrix
 X_mapped, mu, sig = feature_normalization.normalize(X_mapped)
-
 # adding ones
 X_mapped = mat.c_[mat.ones(X_mapped.shape[0]), X_mapped]
 
@@ -115,9 +113,11 @@ print("\n================  Training newly added Polynomial Regression set ======
 lamb = 0
 theta = mat.c_[Rg.optimize_grad(X_mapped, Y, lamb)]
 
+# plotting
 min_x = mat.min(X, axis=0)
 max_x = mat.max(X, axis=0)
 x, x_poly = ployfit.fit(min_x, max_x, mu, sig, power)
+
 pred_y = Rg.hypo(x_poly, theta)
 pyplot.title("Polynomial Regression at lambda = 0")
 pyplot.xlabel('Change in water level (x)')
@@ -131,6 +131,7 @@ pyplot.show()
 print("\n================  Learning Curve for newly added Polynomial Regression ================ ")
 
 error_train1, error_val1, iteration = leaning_curve.curve(X_mapped, Y, X_poly_val, Yval, lamb)
+
 print("Learning curve of poly added feature at lambda =0 ")
 pyplot.xlabel('iteration')
 pyplot.ylabel('error')
@@ -144,14 +145,15 @@ pyplot.show()
 # ======================== Choosing best value of Regularization parameter lambda =====================
 print("\n===================== Choosing best value of Regularization parameter lambda =======================")
 
-# regularization parameter lambda can effect the model in big way. low value (say 0) can result in over fitting that
-# means prediction on training set will going to be well but not going to good on cross validation or test set.
-# too high value (say 100) can result in under fitting case in which the it will not going to perform well on any of the data set
-# so by choosing best lambda value we can make our model more flexible.
+# regularization parameter lambda can effect the model in big way. low value (say 0) can result in overfitting that
+# means prediction on training set will going to be well but not going to accurate on cross validation or test set. too
+# high value (say 100) can result in under fitting case in which the it will not going to perform well on any of the
+# data set so by choosing best lambda value we can make our model more flexible.
 
-# now we are going to calculate weight parameter of training set using multiple parameter (lambda) value then we are going to
-# calculate cost using all those weight parameter on both training and validation set. the weight parameter set which going to give
-# the minimum cost on validation set that value of lambda is going to be the best of all.
+# now we are going to calculate weight parameter of training set using multiple regularization parameter (lambda)
+# value then we are going to calculate cost using all those weight parameter on both training and validation set. the
+# weight parameter set which going to give the minimum cost/error on validation set that value of lambda is going to
+# be the best of all.
 
 lamb_vec = mat.array([0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10])
 cost_train = mat.zeros((len(lamb_vec), 1))
@@ -164,9 +166,9 @@ for i in range(len(lamb_vec)):
     cost_val[i, :] = Rg.cal_cost(theta, X_poly_val, Yval, 0)
 
 # find out minimum error on cross validation set
-min_val = mat.min(cost_val)
-ind = mat.where(cost_val == min_val)[0]
-print("best value of lambda at validation error = ",min_val,"is = ", lamb_vec[ind])
+min_val = mat.min(cost_val) # get the minimum value
+ind = mat.where(cost_val == min_val)[0] # find the minimum value index
+print("best value of lambda at validation error = ", min_val, "is = ", lamb_vec[ind])
 
 print("\n================  Learning Curve for errors ================ ")
 
@@ -181,6 +183,6 @@ pyplot.show()
 # =========================== calculating error on test set using best lambda =======================
 print("\n======================== calculating error on test set using lambda = ", lamb_vec[ind],"================")
 
-initial_theta = mat.zeros(X.shape[1]+1)
-test_error = Rg.cal_cost(initial_theta,mat.c_[mat.ones(Xtest.shape[0]),Xtest], Ytest, lamb_vec[ind])
+theta = Rg.optimize_grad(X_mapped, Y, lamb_vec[ind])
+test_error = Rg.cal_cost(theta, X_poly_test, Ytest, lamb_vec[ind])
 print(test_error)
